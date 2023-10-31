10f8829dc6cfb3a2a9c83f2cb353287047f0018c
vlucet <valentin.lucet@gmail.com>
2023-10-30 21:14:59 -0400

    add apptainer

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/bash
 
 docker build -t rofcamtrap dockerfiles
+docker save rofcamtrap -o rofcamtrap.tar
+# docker image rm rofcamtrap
+apptainer build --fakeroot rofcamtrap.sif docker-archive://rofcamtrap.tar
+rm rofcamtrap.tar
