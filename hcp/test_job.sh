c51d13d0fcd4d939c9f1aa7e53739d2e16fa88c0
vlucet <valentin.lucet@gmail.com>
2023-10-31 10:43:38 -0400

    add cd

--- hcp/test_job.sh
+++ hcp/test_job.sh
@@ -7,5 +7,7 @@
 
 module load apptainer
 
+cd /home/vlucet/projects/rrg-fstewart/vlucet/rofcamtrap
+
 apptainer shell --nv -C -B "$(pwd):/workspace/rofcamtrap" -B \
   "/home/vlucet/projects/rrg-fstewart/vlucet:/workspace/project/" rofcamtrap.sif
