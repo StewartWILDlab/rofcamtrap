3bc87584c92e140903029e21290b44730b37b0fa
vlucet <valentin.lucet@gmail.com>
2023-11-14 17:42:34 -0500

    Add exiftool to dockerfile

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-docker build -t rofcamtrap dockerfiles
+docker build -t rofcamtrap rofcamtrap/dockerfiles
 docker save rofcamtrap -o rofcamtrap.tar
 # docker image rm rofcamtrap
 apptainer build --fakeroot rofcamtrap.sif docker-archive://rofcamtrap.tar
