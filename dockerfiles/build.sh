2c44c91dcda1a42f65aae9a86484c280556b0bd6
vlucet <valentin.lucet@gmail.com>
2023-11-14 23:40:42 -0500

    fix build

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 docker build -t rofcamtrap rofcamtrap/dockerfiles
-docker save rofcamtrap -o rofcamtrap.tar
+docker save rofcamtrap -o rofcamtrap/rofcamtrap.tar
 # docker image rm rofcamtrap
-apptainer build --fakeroot rofcamtrap.sif docker-archive://rofcamtrap.tar
+apptainer build --fakeroot rofcamtrap/rofcamtrap.sif docker-archive://rofcamtrap.tar
 # rm rofcamtrap.tar
