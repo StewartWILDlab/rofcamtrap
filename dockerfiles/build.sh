5397990018457e8ec6404668d4598c13ffbf35c6
vlucet <valentin.lucet@gmail.com>
2023-11-14 23:41:51 -0500

    change commented

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -4,4 +4,4 @@ docker build -t rofcamtrap rofcamtrap/dockerfiles
 docker save rofcamtrap -o rofcamtrap/rofcamtrap.tar
 # docker image rm rofcamtrap
 apptainer build --fakeroot rofcamtrap/rofcamtrap.sif docker-archive://rofcamtrap.tar
-# rm rofcamtrap.tar
+# rm rofcamtrap/rofcamtrap.tar
