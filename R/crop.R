a32f24018b041b94e14c51e504b245a34c9edd7b
vlucet <valentin.lucet@gmail.com>
2024-01-19 16:31:09 -0500

    update crop

--- R/crop.R
+++ R/crop.R
@@ -2,34 +2,41 @@
 
 #' @export
 make_crop_name_label <- function(the_row,
-                                 dest_folder) {
+                                 out_dir) {
   img_name <-
-    paste0(out_folder,
-           stringr::str_replace_all(stringr::str_replace_all(
-             the_row$species, pattern = c(" "), replacement = "_"),
-             pattern = stringr::fixed("."), replacement = ""), "_", the_row$id)
+    file.path(out_dir,
+              paste0(stringr::str_replace_all(stringr::str_replace_all(
+                the_row$species, pattern = c(" "), replacement = "_"),
+                pattern = stringr::fixed("."), replacement = ""), "_", the_row$id,
+                ".JPG")
+    )
   return(img_name)
 }
 
 #' @export
 crop_from_annotations <- function(annotations,
-                                  dest_folder,
+                                  base_dir,
+                                  out_dir,
                                   overwrite = FALSE,
                                   save_anns = TRUE,
                                   save_labels = TRUE) {
+  browser()
+
   annotations <- annotations |>
     dplyr::mutate(crop_name = NA)
 
   for (row_id in seq_len(nrow(annotations))) {
 
-    the_row <- annotations[row,]
+    the_row <- annotations[row_id,]
 
     if (is.na(the_row$species) ||
         (the_row$species %in% c("Human", "Vehicle"))) {
+
       next
+
     } else {
 
-      img_name <- make_crop_name_label(the_row, dest_folder)
+      img_name <- make_crop_name_label(the_row, out_dir)
       annotations[row_id,"crop_name"] <- img_name
 
       if (!overwrite && file.exists(img_name)) {
@@ -40,7 +47,7 @@ crop_from_annotations <- function(annotations,
 
         # print(r$id)
 
-        img <- magick::image_read(paste0(base_folder, the_row$source_file))
+        img <- magick::image_read(file.path(base_dir, the_row$source_file_with_dep))
         img_cropped <- magick::image_crop(img, magick::geometry_area(
           width  = (the_row$value_width/100) * the_row$original_width,
           height = (the_row$value_height/100) * the_row$original_height,
