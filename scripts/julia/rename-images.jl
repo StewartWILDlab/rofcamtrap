4fb7f2340dfee1d5de2bfc77ef945e518ff148fa
vlucet <valentin.lucet@gmail.com>
2024-02-21 12:55:44 -0500

    add par

--- scripts/julia/rename-images.jl
+++ scripts/julia/rename-images.jl
@@ -1,8 +1,66 @@
 
-using Pkg
-Pkg.activate(".")
-using ExifViewer
-using ProgressBars
+@everywhere using Pkg
+@everywhere Pkg.activate(".")
+@everywhere Pkg.instantiate()
+@everywhere using ExifViewer
+@everywhere using ProgressBars
+@everywhere using ProgressMeter
+@everywhere using DataFrames
+@everywhere using CSV
+
+@everywhere function get_datetime(image)
+
+    io = open(image, "r")
+    tags =  read_tags(io, read_all = false, tags=["EXIF_TAG_DATE_TIME_ORIGINAL"])
+    
+    if isempty(tags)
+        @info("Image $image has no datetime")
+        datetime = nothing
+    else
+        datetime = replace(replace(first(tags)[2], ":"=>"_"), " "=>"_")
+    end
+
+    close(io)
+
+    return(datetime)
+
+end
+
+@everywhere function process_image(image, datetime, images_to, deployment_code, loc, loc_base, file_ext)
+
+    if isnothing(datetime) 
+        @info("Image $image has no datetime")
+    else
+        # Filter the path for useless sections, removing DCIM first
+        image_split_filtered = replace.(replace.(replace.(replace.(replace.(
+            filter(x -> x != "DCIM", 
+                # Split at appropriate file separator
+                splitpath(replace(image, loc=>""))[2:end]), 
+            "RECNX"=>""), "RCNX"=>""), file_ext=>""), "Wildlife"=>"W"), "NonWildlife"=>"NW")         
+        
+        # Construct the new file name...
+        new_name =  join([deployment_code, loc_base,
+            join(vcat(image_split_filtered, [datetime]), "_") * ".JPG"], "_")
+
+        # ...and new file path
+        new_path = joinpath(images_to, deployment_code, 
+                            join([deployment_code, loc_base], "_"),
+                            join(vcat([deployment_code], [loc_base], image_split_filtered[1:2]), "_"),
+                            new_name)
+
+        if isfile(new_path)
+            # @info("File $(basename(new_path)) already exists")
+        else
+            if !isdir(dirname(new_path)) 
+                @info("Creating path $(dirname(new_path))")
+                mkpath(dirname(new_path)) 
+            end
+            # @info("Creating file $(basename(new_path))")
+            cp(image, new_path)
+        end
+    end
+
+end
 
 function rename_images(
     images_from,
@@ -43,44 +101,23 @@ function rename_images(
         end
 
         @info("Found $(length(all_images)) images, first is $(all_images[1])")
+        
+        exif_path = joinpath(images_from, (loc_base * "_exif.csv"))
 
-        Threads.@threads for i in ProgressBar(1:length(all_images))
-            image = all_images[i]
-
-            # Filter the path for useless sections, removing DCIM first
-            image_split_filtered = replace.(replace.(replace.(replace.(replace.(
-                filter(x -> x != "DCIM", 
-                    # Split at appropriate file separator
-                    splitpath(replace(image, loc=>""))[2:end]), 
-                "RECNX"=>""), "RCNX"=>""), file_ext=>""), "Wildlife"=>"W"), "NonWildlife"=>"NW")
-            
-            # Get datetime from exif dat
-            io = open(image, "r")
-            datetime = replace(replace(first(read_tags(io, read_all = false, 
-                tags=["EXIF_TAG_DATE_TIME_ORIGINAL"]))[2], ":"=>"_"), " "=>"_")
-            close(io)
-            
-            # Construct the new file name...
-            new_name =  join([deployment_code, loc_base,
-                join(vcat(image_split_filtered, [datetime]), "_") * ".JPG"], "_")
-
-            # ...and new file path
-            new_path = joinpath(images_to, deployment_code, 
-                                join([deployment_code, loc_base], "_"),
-                                join(vcat([deployment_code], [loc_base], image_split_filtered[1:2]), "_"),
-                                new_name)
-
-            if isfile(new_path)
-                # @info("File $(basename(new_path)) already exists")
-            else
-                if !isdir(dirname(new_path)) 
-                    @info("Creating path $(dirname(new_path))")
-                    mkpath(dirname(new_path)) 
-                end
-                # @info("Creating file $(basename(new_path))")
-                cp(image, new_path)
+        if !isfile(exif_path) 
+            @info("Collecting exif datetime")
+            datetimes = @showprogress pmap(all_images) do x
+               get_datetime(x)
             end
-            
+            exif = DataFrame(:image => all_images, :datetime =>datetimes)
+            CSV.write(exif_path, exif)
+        else
+            exif = CSV.read(exif_path, DataFrame)
+        end
+
+        
+        @showprogress pmap(all_images, exif[!, :datetime]) do x, y
+            process_image(x, y, images_to, deployment_code, loc, loc_base, file_ext)
         end
 
     end
@@ -98,4 +135,4 @@ end
 
 rename_images("/home/ubuntu/data/TrailCamStorage", 
               "/home/ubuntu/data/renamed", 
-              "TC1")
+              "TC1"; locations_subset = ["P030"])
