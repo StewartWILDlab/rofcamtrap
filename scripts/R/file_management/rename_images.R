d2829846a9760cf1fbe5adbcb5cd2f9d6fbf7dda
vlucet <valentin.lucet@gmail.com>
2024-02-24 13:57:49 -0500

    updates

--- scripts/R/file_management/rename_images.R
+++ scripts/R/file_management/rename_images.R
@@ -13,3 +13,6 @@ rename_images(images_from = "/media/vlucet/TrailCamST1/TrailCamStorage_2/",
               images_to = "/media/vlucet/TrailCamST1/renamed",
               deployment_code = "TC2",
               file_ext = "\\.(jpg|jpeg|JPG|JPEG)$")
+
+# [1] "P045" "P058" "P080!!!" "P087" "P100"
+# [6] "P216" "P224" "P293" "Q647"
--- scripts/julia/rename-images.jl
+++ scripts/julia/rename-images.jl
@@ -1,43 +1,51 @@
 
+# For reproducibility
 @everywhere using Pkg
 @everywhere Pkg.activate(".")
 @everywhere Pkg.instantiate()
 
+# All workers need ExifViewer to read the exif data
 @everywhere using ExifViewer
 
+# Other modules needed
 using ProgressMeter
 using DataFrames
 using CSV
 
+# Get the Datetime info
 @everywhere function get_datetime(image)
 
+    # Open the file to read a single tag
     io = open(image, "r")
     tags =  read_tags(io, read_all = false, tags=["EXIF_TAG_DATE_TIME_ORIGINAL"])
     
+    # If the tag is empty, throw an error, else format it
     if isempty(tags)
-        @info("Image $image has no datetime")
-        datetime = nothing
+        throw("Image $image has no datetime")
     else
         datetime = replace(replace(first(tags)[2], ":"=>"_"), " "=>"_")
     end
 
+    # Make sure to close the connection to the filte
     close(io)
 
     return(datetime)
 
 end
 
-@everywhere function process_image(image, datetime, images_to, deployment_code, loc, loc_base, file_ext)
-
+# Construct a new path
+@everywhere function make_new_path(image, datetime ; images_to, deployment_code, loc, loc_base, file_ext)
+    
+    # If the datetime is empty, throw an error, else format it
     if isnothing(datetime) 
-        @info("Image $image has no datetime")
+        throw("Image $image has no datetime")
     else
         # Filter the path for useless sections, removing DCIM first
         image_split_filtered = replace.(replace.(replace.(replace.(replace.(
             filter(x -> x != "DCIM", 
                 # Split at appropriate file separator
                 splitpath(replace(image, loc=>""))[2:end]), 
-            "RECNX"=>""), "RCNX"=>""), file_ext=>""), "Wildlife"=>"W"), "NonWildlife"=>"NW")         
+            "RECNX"=>""), "RCNX"=>""), file_ext=>""), "NonWildlife"=>"NW"), "Wildlife"=>"W")         
         
         # Construct the new file name...
         new_name =  join([deployment_code, loc_base,
@@ -48,21 +56,27 @@ end
                             join([deployment_code, loc_base], "_"),
                             join(vcat([deployment_code], [loc_base], image_split_filtered[1:2]), "_"),
                             new_name)
+    end
 
-        if isfile(new_path)
-            # @info("File $(basename(new_path)) already exists")
-        else
-            if !isdir(dirname(new_path)) 
-                @info("Creating path $(dirname(new_path))")
-                mkpath(dirname(new_path)) 
-            end
-            # @info("Creating file $(basename(new_path))")
-            cp(image, new_path, force=true)
+    return(new_path)
+
+end
+
+# Side effect only function to copy files
+@everywhere function process_image(image, new_path; force = true)
+
+    if !isfile(new_path)
+        if !isdir(dirname(new_path)) 
+            @info("Creating path $(dirname(new_path))")
+            mkpath(dirname(new_path)) 
         end
+        # @info("Creating file $(basename(new_path))")
+        cp(image, new_path, force = force)
     end
 
 end
 
+# Main function
 function rename_images(
     images_from,
     images_to,
@@ -83,7 +97,7 @@ function rename_images(
         locations = locations_all
     end
 
-    println(basename.(locations))
+    @info("Processing locations $(basename.(locations))")
 
     # For all locations, proceed to the copy and renaming
     for loc in locations # This is the full path...
@@ -92,41 +106,52 @@ function rename_images(
         loc_base = basename(loc)
         @info("Processing folder $loc_base")
 
-        # TEMP skip if csv present
+        # Make path to exif data
         exif_path = joinpath(images_from, (loc_base * "_exif.csv"))
-        if isfile(exif_path) 
-            @info("Skipping $loc_base")
-            continue
-        end
 
-        # List all images
-        all_images = []
-        for (root, _, files) in walkdir(loc)
-            for file in files
-                image_path =  joinpath(root, file)
-                if occursin(file_ext, image_path) push!(all_images, image_path) end
+        if !isfile(exif_path)
+
+            # List all images
+            @info("Walking folder... this could take a while")
+            all_images = []
+            for (root, _, files) in walkdir(loc)
+                for file in files
+                    image_path =  joinpath(root, file)
+                    if occursin(file_ext, image_path) push!(all_images, image_path) end
+                end
             end
-        end
+            @info("Found $(length(all_images)) images, first is $(all_images[1])")
 
-        @info("Found $(length(all_images)) images, first is $(all_images[1])")
-
-        if !isfile(exif_path) 
             @info("Collecting exif datetime")
             datetimes = @showprogress pmap(all_images) do x
                get_datetime(x)
             end
+
             exif = DataFrame(:image => all_images, :datetime =>datetimes)
             CSV.write(exif_path, exif)
             @info("Datetime data saved")
+
         else
+
             @info("Reading datetime data")
             exif = CSV.read(exif_path, DataFrame)
+
+            all_images = exif[!, :image]
+            datetimes = exif[!, :datetime]
+            @info("Found $(length(all_images)) images, first is $(all_images[1])")
+
+        end
+        
+        # Construct the new paths
+        all_new_paths = @showprogress map(all_images, datetimes) do x, y
+            make_new_path(x, y ; images_to = images_to, deployment_code = deployment_code, 
+                                 loc = loc , loc_base = loc_base, file_ext = file_ext)
         end
 
         @info("Processing files")
-        @showprogress pmap(all_images, exif[!, :datetime]) do x, y
+        @showprogress pmap(all_images, all_new_paths) do from, to
             sleep(0.1)
-            process_image(x, y, images_to, deployment_code, loc, loc_base, file_ext)
+            process_image(from, to)
             sleep(0.1)
         end
 
@@ -134,15 +159,58 @@ function rename_images(
 
 end
 
-# rename_images("/media/vlucet/TrailCamST1/TrailCamStorage", 
-#               "/media/vlucet/TrailCamST1/renamed", 
-#               "TC1"; locations_subset = [  # "P045", "P058", "Q647", "P216", "P100",
-#                                          "P087", "P293",  "P080", "P224" ])
+rename_images("/media/vlucet/TrailCamST1/TrailCamStorage/", 
+              "/home/vlucet/Documents/WILDLab/renamed", 
+            #   "/media/vlucet/TrailCamST1/renamed", 
+              "TC1";  locations_subset = ["P092"])
 
-rename_images("/media/vlucet/TrailCamST1/TrailCamStorage", 
+rename_images("/media/vlucet/TrailCamST1/TrailCamStorage/", 
+              # "/home/vlucet/Documents/WILDLab/renamed", 
               "/media/vlucet/TrailCamST1/renamed", 
-              "TC1")
+              "TC1";  locations_subset = ["P080", "P092"]) # "P080",
+
+rename_images("/media/vlucet/TrailCamST1/TrailCamStorage_2/", 
+              "/media/vlucet/TrailCamST1/renamed", 
+              "TC2")
 
 # rename_images("/home/ubuntu/data/TrailCamStorage", 
 #               "/home/ubuntu/data/renamed", 
 #               "TC1"; locations_subset = ["P030"])
+
+# "P045" "P058" "P080" "P087" "P100"
+# "P216" "P224" "P293" "Q647"
+
+# # TEMP skip if csv present
+# if isfile(exif_path) 
+#     @info("Skipping $loc_base")
+#     continue
+# end
+
+function count_images(images_from,
+                      file_ext = r"\.(jpg|jpeg|JPG|JPEG)$")
+
+    # Get all location folders in this deployment, some will have _repeat or _crop
+    # in them, due to the way folders were processed before renaming
+    locations = filter(isdir, readdir(images_from; join=true)) |> 
+        filter(s -> !occursin("repeat", s)) |>
+        filter(s -> !occursin("crop", s))
+
+    for loc in locations
+        x = 0 ; 
+        for (root, _, files) in walkdir(loc)
+            for file in files
+                image_path =  joinpath(root, file)
+                if occursin(file_ext, image_path) x += 1 end
+            end
+        end
+        println("Loc $(basename(loc)) has $x images")
+    end
+
+end
+
+function check_exif()
+end
+
+count_images("/media/vlucet/TrailCamST1/TrailCamStorage/")
+
+count_images("/media/vlucet/TrailCamST1/renamed/TC1")
